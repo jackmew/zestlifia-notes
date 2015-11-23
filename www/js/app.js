e6eaa3f4a83df6812ac74c037b6d1a452be04adc
Jackmew <wonderfuljack123@gmail.com>
2015-11-23 15:44:08 +0800

    Create new Module for NoteStore

--- www/js/app.js
+++ www/js/app.js
@@ -1,38 +1,6 @@
 (function() {
 
-var app = angular.module('starter', ['ionic']);
-
-app.factory('NoteStore', function() {
-
-  var notes = [] ;
-
-  return {
-
-    list: function() {
-      return notes ;
-    },
-    get: function(noteId) {
-      for(var i = 0 ; i < notes.length ; i++) {
-        if(notes[i].id === noteId) {
-          return notes[i];
-        }
-      }
-      return undefined ;
-    },
-    create: function(note) {
-      notes.push(note);
-    },
-    update: function(note) {
-      for(var i = 0 ; i < notes.length ; i++) {
-        if(notes[i].id === note.id) {
-          notes[i] = note ;
-          return ;
-        }
-      }
-    }
-
-  };
-});
+var app = angular.module('zestlifiaNote', ['ionic','zestlifiaNote.noteStore']);
 
 app.controller('ListCtrl', function($scope, NoteStore) {
   $scope.notes = NoteStore.list() ;
